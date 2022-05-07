167a09227bb8b7f683e066fae66adc538c416447
Mauro Garcia <mauro.garlar@gmail.com>
2022-05-07 18:23:28 -0500

    Cambio 2

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexion con Github se ha hecho");
+		System.out.println("La conexion 2 con Github se ha hecho");
 	}
 
 
